180eaa3a3c77cc0799c0fbaee400dfc93e1fb80a
Kodam-Vinay <vinay.kodam112@gmail.com>
2023-08-10 17:39:29 +0530

    added some change

--- src/components/EachRestaurantCard/index.js
+++ src/components/EachRestaurantCard/index.js
@@ -2,7 +2,7 @@ const EachRestaurantCard = (props) => {
   const { restaurantList } = props;
   const { name } = restaurantList;
   return (
-    <li>
+    <li className="h-52 w-36">
       <h1>{name}</h1>
     </li>
   );
